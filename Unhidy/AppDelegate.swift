d0dd233be0b87d2a17d2a929e0fe01c68c052f56
Daniel Bird <danieldbird@gmail.com>
2017-07-03 19:04:11 +1200

    move menu to status menu and create icon

--- Unhidy/AppDelegate.swift
+++ Unhidy/AppDelegate.swift
@@ -11,10 +11,15 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    @IBOutlet weak var statusMenu: NSMenu!
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        let icon = NSImage(named: "statusIcon")
+        // icon?.isTemplate = true // best for dark mode
+        statusItem.image = icon
+        statusItem.menu = statusMenu
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {

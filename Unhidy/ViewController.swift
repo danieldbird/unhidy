219105dd75c6a62da5f5742f4c8ed5fb45e3456d
Daniel Bird <danieldbird@gmail.com>
2017-07-03 16:20:12 +1200

    initial commit

---
+++ Unhidy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Unhidy
+//
+//  Created by Daniel Bird on 3/07/17.
+//  Copyright © 2017 Daniel Bird. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

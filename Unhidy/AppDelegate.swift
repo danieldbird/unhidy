5c69dfc8b7cdfe3f534bd442dcac117bc48695c8
Daniel Bird <danieldbird@gmail.com>
2017-07-05 09:29:09 +1200

    basic app icon toggle and icon state changes

--- Unhidy/AppDelegate.swift
+++ Unhidy/AppDelegate.swift
@@ -10,16 +10,51 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
     @IBOutlet weak var statusMenu: NSMenu!
+    
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
-
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        let icon = NSImage(named: "statusIcon")
-        // icon?.isTemplate = true // best for dark mode
-        statusItem.image = icon
-        statusItem.menu = statusMenu
+        let filePath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0].appending("/Preferences/com.apple.finder.plist")
+        let myDict: NSDictionary
+        myDict = NSDictionary(contentsOfFile: filePath)!
+        let value = myDict["AppleShowAllFiles"]
+        let state = value as! String
+        
+        if state=="YES" || state=="1" {
+            print("State is: \(state)")
+            let icon = NSImage(named: "showIcon")
+            statusItem.image = icon
+            statusItem.menu = statusMenu
+        } else {
+            print("State is: \(state)")
+            let icon = NSImage(named: "hideIcon")
+            statusItem.image = icon
+            statusItem.menu = statusMenu
+        }
+        
+    }
+    
+    // click toggle menu item
+    @IBAction func clickToggleShowHide(_ sender: NSMenuItem) {
+        let filePath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0].appending("/Preferences/com.apple.finder.plist")
+        let myDict: NSDictionary
+        myDict = NSDictionary(contentsOfFile: filePath)!
+        let value = myDict["AppleShowAllFiles"]
+        let state = value as! String
+        
+        if state=="YES" || state=="1" {
+            let icon = NSImage(named: "hideIcon")
+            statusItem.image = icon
+            statusItem.menu = statusMenu
+        } else {
+            print("State is: \(state)")
+            let icon = NSImage(named: "showIcon")
+            statusItem.image = icon
+            statusItem.menu = statusMenu
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {

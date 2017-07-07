28a5c09d93aac48a03c5fdb5bc1750a3a3fbd146
Daniel Bird <danieldbird@gmail.com>
2017-07-07 17:08:04 +1200

    Complete basic app functionality and ability for users to receive updates in the future.

--- Unhidy/AppDelegate.swift
+++ Unhidy/AppDelegate.swift
@@ -13,54 +13,77 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     
     @IBOutlet weak var statusMenu: NSMenu!
     
-    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    var statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    
+    var statusBar = NSStatusBar.system()
+    var menuItem : NSMenuItem = NSMenuItem()
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
-        let filePath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0].appending("/Preferences/com.apple.finder.plist")
-        let myDict: NSDictionary
-        myDict = NSDictionary(contentsOfFile: filePath)!
-        let value = myDict["AppleShowAllFiles"]
-        let state = value as! String
         
-        if state=="YES" || state=="1" {
-            print("State is: \(state)")
-            let icon = NSImage(named: "showIcon")
-            statusItem.image = icon
-            statusItem.menu = statusMenu
+        // Get state of files hidden
+        let getState = Process()
+        getState.launchPath = "/usr/bin/defaults"
+        getState.arguments = ["read", "com.apple.finder", "AppleShowAllFiles"]
+        let pipe = Pipe()
+        getState.standardOutput = pipe
+        getState.launch()
+        let data = pipe.fileHandleForReading.readDataToEndOfFile()
+        let state = String(data: data, encoding: String.Encoding.utf8)!.trimmingCharacters(in: .newlines)
+        
+        if state=="NO" || state=="0"{
+            statusItem.image = NSImage(named: "hideIcon")
+            statusMenu.insertItem(withTitle: "Show hidden files", action: #selector(AppDelegate.showFiles(_:)), keyEquivalent: "", at: 0)
         } else {
-            print("State is: \(state)")
-            let icon = NSImage(named: "hideIcon")
-            statusItem.image = icon
-            statusItem.menu = statusMenu
+            statusItem.image = NSImage(named: "showIcon")
+            statusMenu.insertItem(withTitle: "Hide hidden files", action: #selector(AppDelegate.hideFiles(_:)), keyEquivalent: "", at: 0)
         }
-        
+        statusItem.menu = statusMenu
     }
     
-    // click toggle menu item
-    @IBAction func clickToggleShowHide(_ sender: NSMenuItem) {
-        let filePath = NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0].appending("/Preferences/com.apple.finder.plist")
-        let myDict: NSDictionary
-        myDict = NSDictionary(contentsOfFile: filePath)!
-        let value = myDict["AppleShowAllFiles"]
-        let state = value as! String
+    @IBAction func hideFiles(_ sender: NSMenuItem) {
+        statusItem.image = NSImage(named: "hideIcon")
+        statusMenu.removeItem(at: 0)
+        statusMenu.insertItem(withTitle: "Show hidden files", action: #selector(AppDelegate.showFiles(_:)), keyEquivalent: "", at: 0)
+        statusItem.menu = statusMenu
+        let setState = Process()
+        setState.launchPath = "/usr/bin/defaults"
+        setState.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "NO"]
+        setState.launch()
         
-        if state=="YES" || state=="1" {
-            let icon = NSImage(named: "hideIcon")
-            statusItem.image = icon
-            statusItem.menu = statusMenu
-        } else {
-            print("State is: \(state)")
-            let icon = NSImage(named: "showIcon")
-            statusItem.image = icon
-            statusItem.menu = statusMenu
+        // ensure state is set before restarting finder
+        let when = DispatchTime.now() + 0.05
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            let killFinder = Process()
+            killFinder.launchPath = "/usr/bin/killall"
+            killFinder.arguments = ["Finder"]
+            killFinder.launch()
+        }
+    }
+    
+    @IBAction func showFiles(_ sender: NSMenuItem) {
+        statusItem.image = NSImage(named: "showIcon")
+        statusMenu.removeItem(at: 0)
+        statusMenu.insertItem(withTitle: "Hide hidden files", action: #selector(AppDelegate.hideFiles(_:)), keyEquivalent: "", at: 0)
+        statusItem.menu = statusMenu
+        let setState = Process()
+        setState.launchPath = "/usr/bin/defaults"
+        setState.arguments = ["write", "com.apple.finder", "AppleShowAllFiles", "YES"]
+        setState.launch()
+        
+        // ensure state is set before restarting finder
+        let when = DispatchTime.now() + 0.05
+        DispatchQueue.main.asyncAfter(deadline: when) {
+            let killFinder = Process()
+            killFinder.launchPath = "/usr/bin/killall"
+            killFinder.arguments = ["Finder"]
+            killFinder.launch()
         }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
 
 }
 
